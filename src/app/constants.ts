d023990e09268045ccfa869d8cb6005cfdc78c54
EfimShikanov <47362413+EfimShikanov@users.noreply.github.com>
2023-05-21 15:02:02 +0000

    Deploying to gh-pages from @ EfimShikanov/gpt-chat@e6dd8f6436436c80488c50a8de188f19214308d1 🚀

--- src/app/constants.ts
+++
@@ -1,1 +0,0 @@
-export const API_KEY = "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";
--- src/app/openai.ts
+++
@@ -1,8 +0,0 @@
-import { Configuration, OpenAIApi } from "openai";
-import { API_KEY } from "./constants";
-
-const configuration = new Configuration({
-  apiKey: API_KEY,
-});
-
-export const openai = new OpenAIApi(configuration);
