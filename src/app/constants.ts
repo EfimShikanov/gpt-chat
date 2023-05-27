634c3257bbd396822071185aaa0b9715ff79c419
Efim Shikanov <shikanovefim@gmail.com>
2023-05-27 19:08:01 +0500

    added open ai token to env

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,1 +1,1 @@
-export const API_KEY = "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";
+export const API_KEY = "";
--- src/app/openai.ts
+++ src/app/openai.ts
@@ -1,8 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
-import { API_KEY } from "./constants";
 
 const configuration = new Configuration({
-  apiKey: API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 export const openai = new OpenAIApi(configuration);
