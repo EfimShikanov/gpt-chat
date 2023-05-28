d246cb88245fdbb6ab0888f792f3b711d9d4c4ff
Efim Shikanov <shikanovefim@gmail.com>
2023-05-28 18:06:04 +0500

    removed user agent

--- src/app/openai.ts
+++ src/app/openai.ts
@@ -4,6 +4,6 @@ const configuration = new Configuration({
   apiKey: process.env.REACT_APP_OPENAI_API_KEY,
 });
 
-console.log(process.env.REACT_APP_OPENAI_API_KEY);
+delete configuration.baseOptions.headers["User-Agent"];
 
 export const openai = new OpenAIApi(configuration);
